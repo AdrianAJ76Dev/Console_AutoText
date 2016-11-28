69778f92d375df691e9433acd2b173dde7a30554
Adrian <AdrianAJ76Dev@gmail.com>
2016-11-28 18:07:35 -0500

    Building simple document added.

--- Console_AutoText/AutoText.cs
+++ Console_AutoText/AutoText.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Drawing;
-using DocumentFormat.OpenXml.Wordprocessing;
+using W=DocumentFormat.OpenXml.Wordprocessing;
 using System.IO;
 
 namespace Console_AutoText
@@ -29,52 +29,67 @@ namespace Console_AutoText
          * i.e. the Media folder.
          * 
          */
-        public static void DisplayInnerXML(string AutoTextName)
+
+        public static void SimpleWordDoc_Create()
         {
-            string relid=null;
-            string templatefullname = @"C:\Users\ajones\Documents\Automation\Prototypes\Sole Source Letter\Templates\Sole Source Letter v4.dotx";
-            using (WordprocessingDocument wrdTemplate = WordprocessingDocument.Open(templatefullname, false))
+            using (WordprocessingDocument NewSimpleDoc = WordprocessingDocument.Create(@"C:\Users\ajones\Documents\Automation\Prototypes\Sole Source Letter\Documents Generated\CB New Simple Document.docx", WordprocessingDocumentType.Document))
             {
-                MainDocumentPart mdpTemplate = wrdTemplate.MainDocumentPart;
-                Document doc = mdpTemplate.Document;
+                MainDocumentPart mdpNewSimpleDoc = NewSimpleDoc.AddMainDocumentPart();
+                mdpNewSimpleDoc.Document = new W.Document(new W.Body(new W.Paragraph(new W.Run(new W.Text()))));
 
-                GlossaryDocument GlossaryDoc =
-                    mdpTemplate.GetPartsOfType<GlossaryDocumentPart>().FirstOrDefault().GlossaryDocument;
-
-                var gDocPartBody = from x in GlossaryDoc.DocParts
-                                   where x.Descendants<DocPartProperties>().FirstOrDefault().DocPartName.Val == AutoTextName
-                                   select x;
-
-                foreach (var docsubpart in gDocPartBody)
-                {
-                    Drawing dr = docsubpart.Descendants<Drawing>().FirstOrDefault();
-                    if (dr != null)
-                    {
-                        relid = dr.Descendants<Blip>().FirstOrDefault().Embed.Value;
-                        Console.WriteLine("Relationship ID = {0}", relid);
-
-                        //var ImagePartandRel = from x in mdpTemplate.Parts
-                        //                      where x.RelationshipId == relid
-                        //                      select x;
-                    }
-                }
-
-                // this code means nothing because the relid, Relationship ID is for the Glossary Part 
-                // and won't be the SAME in the actual template OR the new document
-                Console.WriteLine("Uri = {0}",mdpTemplate.GetPartById(relid).Uri);
-                Console.ReadLine();
-
-                /* How do I retrieve an image part from the glossary document?
-                 * What does that look like in code and as xml?
-                 * How do I replace the current Image Part AND the run with Autotext?
-                 * Ideally it's:
-                 * 1. get DocPart from Glossary
-                 * 2. Insert it into new document
-                 * 3. Get relationship of Image saved with the DocPart
-                 * 4. Replace (or add) the new relationship 
-                 */
-
+                /* Adding image part now */
+                mdpNewSimpleDoc.AddImagePart(ImagePartType.Png);
             }
         }
+
+
+
+        //public static void DisplayInnerXML(string AutoTextName)
+        //{
+        //    string relid=null;
+        //    string templatefullname = @"C:\Users\ajones\Documents\Automation\Prototypes\Sole Source Letter\Templates\Sole Source Letter v4.dotx";
+        //    using (WordprocessingDocument wrdTemplate = WordprocessingDocument.Open(templatefullname, false))
+        //    {
+        //        MainDocumentPart mdpTemplate = wrdTemplate.MainDocumentPart;
+        //        Document doc = mdpTemplate.Document;
+
+        //        GlossaryDocument GlossaryDoc =
+        //            mdpTemplate.GetPartsOfType<GlossaryDocumentPart>().FirstOrDefault().GlossaryDocument;
+
+        //        var gDocPartBody = from x in GlossaryDoc.DocParts
+        //                           where x.Descendants<DocPartProperties>().FirstOrDefault().DocPartName.Val == AutoTextName
+        //                           select x;
+
+        //        foreach (var docsubpart in gDocPartBody)
+        //        {
+        //            Drawing dr = docsubpart.Descendants<Drawing>().FirstOrDefault();
+        //            if (dr != null)
+        //            {
+        //                relid = dr.Descendants<Blip>().FirstOrDefault().Embed.Value;
+        //                Console.WriteLine("Relationship ID = {0}", relid);
+
+        //                //var ImagePartandRel = from x in mdpTemplate.Parts
+        //                //                      where x.RelationshipId == relid
+        //                //                      select x;
+        //            }
+        //        }
+
+        //        // this code means nothing because the relid, Relationship ID is for the Glossary Part 
+        //        // and won't be the SAME in the actual template OR the new document
+        //        Console.WriteLine("Uri = {0}",mdpTemplate.GetPartById(relid).Uri);
+        //        Console.ReadLine();
+
+        //        /* How do I retrieve an image part from the glossary document?
+        //         * What does that look like in code and as xml?
+        //         * How do I replace the current Image Part AND the run with Autotext?
+        //         * Ideally it's:
+        //         * 1. get DocPart from Glossary
+        //         * 2. Insert it into new document
+        //         * 3. Get relationship of Image saved with the DocPart
+        //         * 4. Replace (or add) the new relationship 
+        //         */
+
+        //    }
+        //}
     }
 }
--- Console_AutoText/Program.cs
+++ Console_AutoText/Program.cs
@@ -14,7 +14,8 @@ namespace Console_AutoText
     {
         static void Main(string[] args)
         {
-           AutoText.DisplayInnerXML("Jeremy Singer");
+            AutoText.SimpleWordDoc_Create();
+           //AutoText.DisplayInnerXML("Jeremy Singer");
         }
     }
 }
